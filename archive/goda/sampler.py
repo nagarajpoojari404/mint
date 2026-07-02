145ee8fcc5091b5f576ceeda6ceb6e10d3792e47
= <=>
2026-07-02 14:17:13 +0530

    remove archive

--- archive/goda/sampler.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-
-class Sampler(ABC):
-
-    @abstractmethod
-    def sample(self, num_samples: int = 1) -> list[dict[str, Any]]:
-        ...
